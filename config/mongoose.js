8baa3025666c845bf3159cb6716923d555ccfb6e
mrOpenMinded <maapaa1073@gmail.com>
2023-10-31 00:02:10 +0530

    added all basic features required for this habit_tracker project

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //connect to the database
 // 'mongodb://127.0.0.1:27017/habit_tracker'
-mongoose.connect('mongodb://127.0.0.1:27017/habit_tracker');
+mongoose.connect('mongodb+srv://maapaa1073:NglDU4QWWJ2Wp012@cluster0.aei2dmf.mongodb.net/?retryWrites=true&w=majority');
 
 //acquire the connection(to check if it's successful)
 const db = mongoose.connection;
@@ -19,3 +19,6 @@ db.once('open', function() {
 });
 
 module.exports=db;
+
+
+// NglDU4QWWJ2Wp012

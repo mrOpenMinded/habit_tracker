97be89b3a5ad0ab2d81adf4402efd809ee1564b2
mrOpenMinded <maapaa1073@gmail.com>
2023-10-31 00:19:28 +0530

    abc

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(session({
         maxAge: (1000 * 60 * 100)
     },
     store: MongoStore.create({
-            mongoUrl: 'mongodb://127.0.0.1:27017/habit_tracker',
+            mongoUrl: process.env.MONGODB_URI,
             autoRemover: 'disabled'
         },
         function(err) {
